81195b981e23c6d3878c0edce7c5a7483a04dcd2
mpavlovska <marijapavlovska01@gmail.com>
2023-08-17 10:58:29 +0200

    Lab 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lime,
       ),
+      home: const MyHomePage(title: '193053 - Marija Pavlovska'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          color: Colors.grey,
+          child: const Center(
+            child: Text(
+              "MIS Lab 1",
+              style: TextStyle(fontSize: 45),
+            ),
+          ),
+        ));
   }
 }
