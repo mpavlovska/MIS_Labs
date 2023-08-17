b781737ac2aeaa2f67c9c0751ed4031b17b7e179
mpavlovska <marijapavlovska01@gmail.com>
2023-08-17 11:14:02 +0200

    Lab 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './choice.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyAppState extends State<MyApp> {
+  void iWasTapped() => print('I was tapped');
+  var questions = [
+    {
+      "question": "Select clothes category:",
+      "answers": ["Sportswear", "Casual wear", "Business attire"],
+    },
+    {
+      "question": "What size do you need?",
+      "answers": [
+        "XS",
+        "S",
+        "M",
+        "L",
+        "XL",
+        "XXL",
+      ],
+    },
+    {
+      "question": "Which color should be the dominant one?",
+      "answers": ["White", "Black", "Grey", "Yellow", "Blue", "Pink"]
+    }
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex < questions.length - 1) {
+        _questionIndex += 1;
+      } else {
+        _questionIndex = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "Hello World",
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Hello World"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]['question'].toString()),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return QuestionChoice(
+                _answerQuestion,
+                answer,
+              );
+            })
+          ],
         ),
       ),
     );
