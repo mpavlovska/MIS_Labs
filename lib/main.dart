e4239a6eea942858578893bcd2f23cca5cbcdfcf
mpavlovska <marijapavlovska01@gmail.com>
2023-08-17 15:18:05 +0200

    Lab 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './LoginWidget.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'MyApp.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MaterialApp(
+    title: 'ExamList App',
+    home: MainPage(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const MyApp();
+            } else {
+              return const LoginWidget();
+            }
+          },
         ),
-      ),
-    );
-  }
+      );
 }
